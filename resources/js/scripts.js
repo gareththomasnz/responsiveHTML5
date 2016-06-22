2ce243a135fb035cf6800976d01cc277cdb06c2a
gareththomasnz <gareththomasnz@yahoo.com>
2016-06-22 22:04:39 +1200

    twentieth  commit

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -36,5 +36,60 @@ $(document).ready(function() {
           }
         });
       });
+    
+    /* animate upon scroll */
+    
+   $('.js--wp-1').waypoint(function(direction){
+  		$('.js--wp-1').addClass('animated fadeIn');
+  }, {
+  	offset: '50%;'
+  });
+   
+ 
+    $('.js--wp-2').waypoint(function(direction){
+  		$('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+  	offset: '50%;'
+  });
 
+     $('.js--wp-3').waypoint(function(direction){
+  		$('.js--wp-3').addClass('animated fadeIn');
+  }, {
+  	offset: '50%;'
+  });
+
+      $('.js--wp-4').waypoint(function(direction){
+  		$('.js--wp-4').addClass('animated pulse');
+  }, {
+  	offset: '50%;'
+  });
+     $('.js--wp-5').waypoint(function(direction){
+  		$('.js--wp-5').addClass('animated pulse');
+  }, {
+  	offset: '50%;'
+  });
+     $('.js--wp-6').waypoint(function(direction){
+  		$('.js--wp-6').addClass('animated pulse');
+  }, {
+  	offset: '50%;'
+  });
+  
+        /* nav animation */
+
+    $('.js--nav-icon').click(function() {
+  	    var nav = $('.js--main-nav');
+  	    var icon = $('.js--nav-icon i');
+
+  	    nav.slideToggle(200);
+  			
+  	    if(icon.hasClass('ion-navicon-round')) {
+  		    icon.addClass('ion-close-round');
+  		    icon.removeClass('ion-navicon-round');
+  	    } else {
+  		    icon.addClass('ion-navicon-round');
+  		    icon.removeClass('ion-close-round');
+  	    }
+  			
+    });
+    
 });
